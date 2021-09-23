4ecd405c9020f035df1e88a9c45af811e507f3c1
Logesh Krishna <logeshkrishna08@gmail.com>
2021-09-23 19:17:50 +0530

    Thu 09/23/2021

---
+++ Prime Pair - Middle Composite.py
@@ -0,0 +1,58 @@
+# Prime Pair - Middle Composite
+# The program must accept an integer N as the input. The program must find two consecutive prime integers X and Y, where the middle integer in the range (X, Y) is equal to 
+# N (i.e., N is a composite integer which is equidistant from two consecutive primes X and Y). If there is no such prime pair, then the program must print -1 as the output.
+
+# Boundary Condition(s):
+# 2 <= N <= 10^5
+
+# Input Format:
+# The first line contains N.
+
+# Output Format:
+# The first line contains X and Y separated by a space or -1.
+
+# Example Input/Output 1:
+# Input:
+# 6
+
+# Output:
+# 5 7
+
+# Explanation:
+# Here N = 6, which is a composite integer.
+# So the prime pair is (5, 7).
+# 6-5 = 7-6 = 1.
+
+# Example Input/Output 2:
+# Input:
+# 473
+
+# Output:
+# 467 479
+
+# Example Input/Output 3:
+# Input:
+# 10
+
+# Output:
+# -1
+
+
+def getPrimeNumber(x):
+    i=n
+    while True:
+        isPrime=True
+        for j in range(2,i):
+            if i%j==0:
+                isPrime=False
+                break
+        if isPrime:
+            return i
+        i+=x
+n=int(input())
+a=getPrimeNumber(-1)
+b=getPrimeNumber(1)
+if (a+b)//2==n and a!=n:
+    print(a,b)
+else:
+    print(-1)
