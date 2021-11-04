3d53b064acab840db003fa1e47d07fa18fb0a594
Logesh <logeshkrishna08@gmail.com>
2021-11-04 22:21:37 +0530

    04-11-2021

---
+++ Powers of 2 - Binary Representation.py
@@ -0,0 +1,44 @@
+# Powers of 2 - Binary Representation
+# The program must accept an integer N as the input. The program must find the binary representation of N. Then the program must print the powers of two indicated by the set bits in the binary representation of N as shown in the Example Input/Output section.
+
+# Boundary Condition(s):
+# 1 <= N <= 10^18
+
+# Input Format:
+# The first line contains N.
+
+# Output Format:
+# The first line contains the powers of two indicated by the set bits in the binary representation of N.
+
+# Example Input/Output 1:
+# Input:
+# 10
+
+# Output:
+# 8+2
+
+# Explanation:
+# Here N = 10 whose binary representation is 1010.
+# 1 -> 1*2^3 = 8
+# 0 -> 0*2^2 = 0
+# 1 -> 1*2^1 = 2
+# 0 -> 0*2^0 = 0
+# Hence the output is 8+2.
+
+# Example Input/Output 2:
+# Input:
+# 15
+
+# Output:
+# 8+4+2+1
+
+# Example Input/Output 3:
+# Input:
+# 83
+
+# Output:
+# 64+16+2+1
+
+
+
+
