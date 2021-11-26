0b671202018a3e9480deb329ee0d928518a5d6de
Logesh <logeshkrishna08@gmail.com>
2021-11-26 22:25:31 +0530

    26-11-2021

---
+++ function getStringFromParentheses.c
@@ -0,0 +1,50 @@
+// function getStringFromParentheses
+// The function/method getStringFromParentheses accepts an argument str representing a string value. The string str contains alphabets and a pair of parentheses.
+
+// The function/method getStringFromParentheses must return a string containing all the characters between the open parenthesis and the close parenthesis in the given string. If there are no characters between the pair of parentheses, then the function must return -1 as a string value. Consider string in circular fashion when finding the characters between the open parenthesis and the close parenthesis.
+
+// Your task is to implement the function getStringFromParentheses so that it passes all the test cases.
+
+// IMPORTANT: Do not write the main() function as it is already defined.
+
+// Example Input/Output 1:
+// Input:
+// abcd(SkillRack)pqrs
+
+// Output:
+// SkillRack
+
+// Explanation:
+// Here the given string value is abcd(SkillRack)pqrs.
+// All the characters between the pair of parentheses are SkillRack.
+// So SkillRack is printed as the output.
+
+// Example Input/Output 2:
+// Input:
+// abcdef)ghij(klmno
+
+// Output:
+// klmnoabcdef
+
+// Example Input/Output 3:
+// Input:
+// bank()account
+
+// Output:
+// -1
+
+// Example Input/Output 4:
+// Input:
+// )donkey(
+
+// Output:
+// -1
+
+
+
+
+
+
+
+
+
