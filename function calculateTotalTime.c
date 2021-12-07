13890b51c5609e195e369c8f4f1015a8f8398e41
Logesh <logeshkrishna08@gmail.com>
2021-12-07 22:18:08 +0530

    07-12-2021

---
+++ function calculateTotalTime.c
@@ -0,0 +1,44 @@
+// function calculateTotalTime
+// The function/method calculateTotalTime accepts an argument str. The string str contains multiple integers separated by a space, where each integer ends with a character denoting the unit of time.
+// d indicates days.
+// h indicates hours.
+// m indicates minutes.
+// s indicates seconds.
+
+// The function/method calculateTotalTime must return the total time in days, hours, minutes and seconds.
+
+// Your task is to implement the function calculateTotalTime so that the program runs successfully.
+
+// The following structure is used to represent the Time and is already defined in the default code (Do not write this definition again in your code).
+// struct Time
+// {
+//     int days;
+//     int hours;
+//     int minutes;
+//     int seconds;
+// };
+// IMPORTANT: Do not write the main() function as it is already defined.
+
+// Example Input/Output 1:
+// Input:
+// 2h 1d 50m 102s 5d 30s 70m
+
+// Output:
+// 6 4 2 12
+
+// Explanation:
+// 2h -> 2 hours
+// 1d -> 1 day
+// 50m -> 50 minutes
+// 102s -> 102 seconds
+// 5d -> 5 days
+// 30s -> 30 seconds
+// 70m -> 70 minutes
+// Total time: 6 days, 4 hours, 2 minutes and 12 seconds.
+
+// Example Input/Output 2:
+// Input:
+// 15d 500s 30h 60s
+
+// Output:
+// 16 6 9 20
