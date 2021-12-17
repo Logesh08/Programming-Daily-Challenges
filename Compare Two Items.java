48bb21a023191463f880939123baedde86f0c0ca
Logesh <logeshkrishna08@gmail.com>
2021-12-17 12:46:54 +0530

    17-12-2021

---
+++ Compare Two Items.java
@@ -0,0 +1,68 @@
+// Compare Two Items
+// The program must accept the name, weight and price of two items as the input. The program must print the string value "SAME" if both items have the same name and the same weight or price. Else the program must print the string value "NOT SAME" as the output. Then the program must print the name, weight and price of the two items as shown in the Example Input/Output section.
+
+// Note: Ignore the case when comparing the names of the items.
+
+// Your task is to define the class Item so that the program runs successfully.
+
+// Example Input/Output 1:
+// Input:
+// Mutton
+// 2
+// 2200
+// Mutton
+// 2
+// 1900
+
+// Output:
+// SAME
+// Mutton 2 2200
+// Mutton 2 1900
+
+// Explanation:
+// Both items have the same name and the same weight.
+
+// Example Input/Output 2:
+// Input:
+// mutton
+// 3
+// 2000
+// MUTTON
+// 2
+// 2000
+
+// Output:
+// SAME
+// mutton 3 2000
+// MUTTON 2 2000
+
+// Explanation:
+// Both items have the same name and the same price.
+
+// Example Input/Output 3:
+// Input:
+// Mutton
+// 2
+// 1800
+// Mutton
+// 1
+// 1000
+
+// Output:
+// NOT SAME
+// Mutton 2 1800
+// Mutton 1 1000
+
+// Example Input/Output 4:
+// Input:
+// Mutton
+// 2
+// 2200
+// Fish
+// 2
+// 1900
+
+// Output:
+// NOT SAME
+// Mutton 2 2200
+// Fish 2 1900
