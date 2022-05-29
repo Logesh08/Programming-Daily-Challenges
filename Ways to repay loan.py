b2bfec955e35dd20e66ef9bb0db7d0d4ae9f99f2
Logesh <logeshkrishna08@gmail.com>
2022-05-29 22:07:10 +0530

    29-05-2022

---
+++ Ways to repay loan.py
@@ -0,0 +1,57 @@
+# Ways to repay loan
+# A poor man borrows a sum of N rupees from his neighbour. The neighbour is kind enough not to impose any interest on the loan. The poor man agrees to re-pay the loan by paying back either 1 rupee or 2 rupees to his neighbour. In how many distinct ways can he repay the loan?
+
+# Input Format:
+# The first line will contain the value of N which is the loan amount.
+
+# Output Format:
+# The first line will contain the number of distinct ways in which the loan can be repaid.
+
+# Constraints:
+# 1 <= N <= 999999
+
+# Example Input/Output 1:
+# Input:
+# 1
+
+# Output:
+# 1
+
+# Example Input/Output 2:
+# Input:
+# 2
+
+# Output:
+# 2
+
+# Explanation:
+# The loan can be re-paid as 1,1 or as 2
+
+# Example Input/Output 3:
+# Input:
+# 5
+
+# Output:
+# 8
+
+# Explanation:
+# The loan can be re-paid as
+# 1,1,1,1,1 or
+# 1,1,1,2 or
+# 1,2,2 or
+# 1,1,2,1 or
+# 1,2,1,1 or
+# 2,2,1 or
+# 2,1,2 or
+# 2,1,1,1   
+
+
+
+
+
+
+
+
+
+
+
