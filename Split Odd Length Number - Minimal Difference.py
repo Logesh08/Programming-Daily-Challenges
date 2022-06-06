f46d2062479bb84c1bfc150dcc98fc5d6b7a446e
Logesh <logeshkrishna08@gmail.com>
2022-06-06 22:09:16 +0530

    06-06-2022

---
+++ Split Odd Length Number - Minimal Difference.py
@@ -0,0 +1,32 @@
+# Split Odd Length Number - Minimal Difference
+# A positive number N of odd length is passed as input. The program must use the digits present in the number and form two numbers A and B such that A-B is minimal. N will contain at least two non zero digits.
+
+# Note: The count of the digits from 0 to 9 present in both A and B should match the exact count of that digit as in N. As an example if the input is 1223000, then 1 can be used only once, 2 can be used twice, 3 can be used once, 0 can be used thrice to form A and B.
+
+# Input Format:
+# The first line contains N.
+
+# Output Format:
+# The first line contains A.
+# The second line contains B.
+
+# Boundary Conditions:
+# 11 <= N <= 9999999999999
+# B < A
+
+# Example Input/Output 1:
+# Input:
+# 13984
+
+# Output:
+# 134
+# 98
+
+# Example Input/Output 2:
+# Input:
+# 1694449
+
+# Output:
+# 1444
+# 996
+
