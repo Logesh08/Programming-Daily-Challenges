9d1901e72bf5f00fefed599dc2c53d41309ba75f
Logesh <logeshkrishna08@gmail.com>
2022-07-24 22:30:30 +0530

    24-07-2022

---
+++ Merge N Pairs of String.py
@@ -0,0 +1,42 @@
+# Merge N Pairs of String
+
+# The program must accept an integer N and N pairs of strings. Then the program must merge every pair of strings and print the merged string.
+
+# Boundary Condition(s):
+# 2 <= N <= 100
+# 1 <= Length of String <= 1000
+
+# Input Format:
+# The first line contains N.
+# The next N lines contain two strings each.
+
+# Output Format:
+# The first N lines contain the merged strings.
+
+# Example Input/Output 1:
+# Input:
+# 2
+# good bye
+# take care
+
+# Output:
+# gboyoed
+# tcaakree
+
+# Example Input/Output 2:
+# Input:
+# 3
+# cool buddy
+# think well
+# welcome tin
+
+# Output:
+# cbouodldy
+# twheilnlk
+# wteilncome
+
+
+
+
+
+
