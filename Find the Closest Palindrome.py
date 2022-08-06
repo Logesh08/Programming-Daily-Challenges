f186699219e210d19840b0041a9b8f73e83da64f
Logesh <logeshkrishna08@gmail.com>
2022-08-06 22:35:16 +0530

    06-08-2022

---
+++ Find the Closest Palindrome.py
@@ -0,0 +1,27 @@
+# Find the Closest Palindrome
+
+# The program must accept a number N as the input. The program must print the closest palindromic number as the output. If there are two closest palindromic numbers the program must print the smaller number.
+
+# Boundary Condition(s):
+# 2 <= N <= 99999999999999
+
+# Input Format:
+# The first line contains the value of N.
+
+# Output Format:
+# The first line contains the closest palindromic number.
+
+# Example Input/Output 1:
+# Input:
+# 124
+
+# Output:
+# 121
+
+# Example Input/Output 2:
+# Input:
+# 1439
+
+# Output:
+# 1441
+
