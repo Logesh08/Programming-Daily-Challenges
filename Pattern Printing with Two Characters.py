1ed9e72496de169c6a1ecd4a6ffec4c5abc85636
Logesh08 <logeshkrishna08@gmail.com>
2022-09-01 22:10:04 +0530

    31-08-2002

---
+++ Pattern Printing with Two Characters.py
@@ -0,0 +1,35 @@
+# Pattern Printing with Two Characters
+
+# The program must accept an integer N and two characters as input. The program must print the desired pattern as shown in the Example Input/Output section.
+
+# Boundary Condition(s):
+# 1 <= N <= 50
+
+# Input Format:
+# The first line contains the value of N followed by a space and then the two characters(without any space between the two characters).
+
+# Output Format:
+# The first N lines contain the desired pattern as shown in the Example Input/Output section.
+
+# Example Input/Output 1:
+# Input:
+# 5 $#
+
+# Output:
+#     $
+#    #$#
+#   $#$#$
+#  #$#$#$#
+# $#$#$#$#$
+
+# Example Input/Output 2:
+# Input:
+# 6 @%
+
+# Output:
+#      @
+#     %@%
+#    @%@%@
+#   %@%@%@%
+#  @%@%@%@%@
+# %@%@%@%@%@%
